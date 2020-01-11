a083e5e48150d6d4c222dc8328292a1f40cfad8d
Alex Parlett <alexparlett@gmail.com>
2020-01-11 20:13:11 +0000

    Enable version autoincrement

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject email-normalisation "1.0.0"
+(defproject email-normalisation "1.0.1"
   :description "Email normalisation using dns over https for provider calculations"
   :url "https://github.com/alexparlett/email-normalisation-clojure"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -10,4 +10,5 @@
   :deploy-repositories {"releases" {:url "https://repo.clojars.org" :creds :env :sign-releases false}}
   :repositories [["releases" {:url "https://repo.clojars.org"
                               :username :env
-                              :password :env}]])
+                              :password :env}]]
+  :plugins [[lein-bump-version "0.1.6"]])
